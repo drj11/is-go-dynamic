c8cf76248db48f818ccb363691a93e80ac38fcda
David Jones <drj@pobox.com>
2015-06-19 19:34:17 +0100

    map example

---
+++ 040/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	spell := map[int]string{1: "one", 2: "two", 4: "four"}
+	fmt.Println(spell[1], spell[2], spell[3], spell[4])
+
+	three, ok := spell[3]
+	fmt.Println(three, ok)
+
+	fmt.Println(len(spell))
+}
