e06cc26ea8df44a1c63d1cab07536d3de85d59cb
David Jones <drj@pobox.com>
2015-06-19 18:49:17 +0100

    Add go code

---
+++ 030/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	slice := []int{1, 2, 3}
+	fmt.Println(slice)
+
+	fmt.Println(len(slice))
+
+	extra := []int{4, 5, 6}
+	slice = append(slice, extra...)
+	fmt.Println(slice)
+
+	slice = append([]int{0}, slice...)
+	fmt.Println(slice)
+	fmt.Println(len(slice))
+
+	slice[0] = 99
+	fmt.Println(slice)
+
+	slice[7] = 99
+	fmt.Println(slice)
+}
